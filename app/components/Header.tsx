aeb9a82cca668980ac732c3538c760bf0baa749f
kou72 <genpokopokopoko@gmail.com>
2023-01-03 03:56:01 +0900

    Headerデザイン作成

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,37 @@
-export default function Header() {
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './header.module.css'
+
+const Header = (props: any) => {
   return (
-    <>
-      <p>header</p>
-    </>
+    <div className={` ${styles['container']} ${styles[props.rootClassName]} `}>
+      <header data-role="Header" className={styles['header']}>
+        <img
+          alt="pastedImage"
+          src="/playground_assets/homies-logo.png"
+          className={styles['icon']}
+        />
+        <div className={styles['nav']}>
+          <span className={styles['home']}>Home</span>
+          <span className={styles['events']}>Events</span>
+          <span className={styles['tickets']}>Tickets</span>
+        </div>
+        <img
+          alt="pastedImage"
+          src="/playground_assets/user-icon-sample.png"
+          className={styles['user-icon']}
+        />
+      </header>
+    </div>
   )
 }
+
+Header.defaultProps = {
+  rootClassName: '',
+}
+
+Header.propTypes = {
+  rootClassName: PropTypes.string,
+}
+
+export default Header

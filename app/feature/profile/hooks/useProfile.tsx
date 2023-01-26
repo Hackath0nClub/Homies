c0377042295cb515aa15aee51278573c9ceef6b7
kou72 <genpokopokopoko@gmail.com>
2023-01-26 20:21:37 +0900

    git merge create-profile

--- app/feature/profile/hooks/useProfile.tsx
+++ app/feature/profile/hooks/useProfile.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import { selectProfileById } from '../infrastructure/profileRepository'
 
-export type Profile = {
-  id: string
-  name: string
-  text: string
-  icon_url: string | null
-  twitter_url: string | null
-  soundcloud_url: string | null
-  mixcloud_url: string | null
-  create_at: Date | null
-  updated_at: Date | null
-}
+export type Profile =
+  | {
+      uuid: string
+      id: string
+      name: string | null
+      icon_url: string | null
+      text: string | null
+      twitter_url: string | null
+      soundcloud_url: string | null
+      mixcloud_url: string | null
+      create_at: Date | null
+      updated_at: Date | null
+    }
+  | undefined
 
 export const useCounter = () => {
   const [count, setCount] = useState(1)

f16b8e85ce32695ebcb111ab6ae4cfe4ce2659a6
kou72 <genpokopokopoko@gmail.com>
2023-02-17 00:42:20 +0900

    タイムテーブルに一行追加する処理完成

--- app/feature/event/components/EditDjTimeTableRow.tsx
+++ app/feature/event/components/EditDjTimeTableRow.tsx
@@ -11,52 +11,85 @@ type propsType = {
 }
 
 export const EditDjTimeTableRow = (props: propsType) => {
+  const length = props.timetable.length
+  const [keywords, setKeywords] = useState<string[]>(Array(length).fill(''))
+  const [results, setResults] = useState<Users[]>(Array(length).fill([]))
+  const [isOpens, setIsOpens] = useState<boolean[]>(Array(length).fill(false))
+  const [timer, setTimer] = useState<any>()
+
+  useEffect(() => {
+    clearTimeout(timer)
+    const newTimers = keywords.map((inputValue, index) => {
+      if (!inputValue) return null
+      return setTimeout(async () => {
+        const users = await props.searchUser(inputValue)
+        const newResults = [...results]
+        newResults[index] = users
+        setResults(newResults)
+      }, 500)
+    })
+    setTimer(newTimers)
+  }, [keywords])
+
+  const addEmptyElement = () => {
+    const newTimetable = [
+      ...props.timetable,
+      {
+        row_number: props.timetable.length + 1,
+        user_id: null,
+        name: '',
+        text: null,
+        icon_url: null,
+        start_time: null,
+        end_time: null,
+      },
+    ]
+    props.setTimetable(newTimetable)
+    setKeywords([...keywords, ''])
+    setResults([...results, []])
+    setIsOpens([...isOpens, false])
+  }
+
   return (
     <>
-      <p className="w-full text-white font-bold text-2xl text-center my-4">
-        タイムテーブル
-      </p>
+      <div className="w-full text-center">
+        <p className="w-full text-white font-bold text-2xl text-center my-4">
+          タイムテーブル
+        </p>
+        <button
+          className="px-4 py-2 mb-4 text-white transition-colors duration-300 border border-gray-200 bg-[rgba(28,32,37,1)] rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+          onClick={addEmptyElement}
+        >
+          行追加
+        </button>
+      </div>
 
       {props.timetable.map((row, index) => {
-        const [inputValue, setInputValue] = useState('')
-        const [results, setResults] = useState<Users>([])
-        const [isOpen, setIsOpen] = useState(false)
-        const [timer, setTimer] = useState<any>()
-
-        useEffect(() => {
-          clearTimeout(timer)
-
-          if (!inputValue) {
-            setResults([])
-            return
-          }
-
-          const newTimer = setTimeout(async () => {
-            const users = await props.searchUser(inputValue)
-            setResults(users)
-          }, 500)
-
-          setTimer(newTimer)
-        }, [inputValue])
+        const inputValue = keywords[index]
+        const isOpen = isOpens[index]
+        const result = results[index]
 
         const handleInputChange = (event: any) => {
-          setInputValue(event.target.value)
-          setIsOpen(true)
+          const newInputValues = [...keywords]
+          newInputValues[index] = event.target.value
+          setKeywords(newInputValues)
+
+          const newIsOpens = [...isOpens]
+          newIsOpens[index] = true
+          setIsOpens(newIsOpens)
         }
 
         const updateItem = (index: number, user: User) => {
-          setInputValue(user.id)
-          setIsOpen(false)
-
+          const newInputValues = [...keywords]
+          const newIsOpens = [...isOpens]
           const newItems = [...props.timetable]
-          const updatedItem = {
-            ...newItems[index],
-            user_id: user.id,
-            name: user.name,
-            icon_url: user.icon_url,
-            text: user.text,
-          }
-          newItems[index] = updatedItem
+
+          newInputValues[index] = user.id
+          newIsOpens[index] = false
+          newItems[index] = { ...newItems[index], ...user }
+
+          setKeywords(newInputValues)
+          setIsOpens(newIsOpens)
           props.setTimetable(newItems)
         }
 
@@ -129,9 +162,10 @@ export const EditDjTimeTableRow = (props: propsType) => {
                       value={inputValue}
                       onChange={handleInputChange}
                     />
+
                     {isOpen && results.length > 0 && (
                       <ul className="absolute z-10 bg-[rgba(47,51,56,1)] mt-2 py-2 rounded-lg shadow-xl">
-                        {results.slice(0, 5).map((user) => (
+                        {result.map((user) => (
                           <li
                             key={user.id}
                             className="flex align-items-center py-2 px-4 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"

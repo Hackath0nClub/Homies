239086df8940bdb8295e676e68e4fc770a602dd9
yotubarail <shota_t@i.softbank.jp>
2023-03-21 17:15:04 +0900

    プロフィール内の切り替えタブのベースを実装

--- app/feature/profile/components/UserEvent.tsx
+++ app/feature/profile/components/UserEvent.tsx
@@ -1,5 +1,98 @@
+import { useState } from "react"
+
 export const UserEvent = (props: any) => {
-  return(
-    <></>
+  const [openTab, setOpenTab] = useState(1)
+  return (
+    <>
+      <div>
+        <div className="profile-event-tab flex flex-wrap">
+          <div className="w-full">
+            <ul
+              className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+              role="tablist"
+            >
+              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg block leading-normal " +
+                    (openTab === 1
+                      ? "text-white border-b-2 border-b-orange-600"
+                      : "text-white")
+                  }
+                  onClick={e => {
+                    e.preventDefault();
+                    setOpenTab(1);
+                  }}
+                  data-toggle="tab"
+                  href="#link1"
+                  role="tablist"
+                >
+                  概要/外部リンク
+                </a>
+              </li>
+              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg block leading-normal " +
+                    (openTab === 2
+                      ? "text-white border-b-2 border-b-orange-600"
+                      : "text-white")
+                  }
+                  onClick={e => {
+                    e.preventDefault();
+                    setOpenTab(2);
+                  }}
+                  data-toggle="tab"
+                  href="#link2"
+                  role="tablist"
+                >
+                  出演イベント
+                </a>
+              </li>
+              <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-5 py-3 shadow-lg block leading-normal " +
+                    (openTab === 3
+                      ? "text-white border-b-2 border-b-orange-600"
+                      : "text-white")
+                  }
+                  onClick={e => {
+                    e.preventDefault();
+                    setOpenTab(3);
+                  }}
+                  data-toggle="tab"
+                  href="#link3"
+                  role="tablist"
+                >
+                  主催イベント
+                </a>
+              </li>
+            </ul>
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+              <div className="px-4 py-5 flex-auto">
+                <div className="tab-content tab-space">
+                  <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+                    <p>
+                      ここにイベント入るよ
+                    </p>
+                  </div>
+                  <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+                    <p>
+                      ここに出演したイベントが入るよ
+                    </p>
+                  </div>
+                  <div className={openTab === 3 ? "block" : "hidden"} id="link3">
+                    <p>
+                      ここに主催したイベントが入るよ
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }

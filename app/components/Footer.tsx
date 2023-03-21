b143bffebf0cfa07954c1da72614f595dec61e1c
yotubarail <shota_t@i.softbank.jp>
2023-03-21 15:15:46 +0900

    フッターのベースになるファイルを作成

---
+++ app/components/Footer.tsx
@@ -0,0 +1,23 @@
+const Footer = (props: any) => {
+  return (
+    <>
+      <div>
+        <footer>
+          <div>
+            <p>利用規約</p>
+            <p>プライバシーポリシー</p>
+            <p>Cookieのポリシー</p>
+            <p>アクセシビリティ</p>
+          </div>
+          <div>
+            <p>&copy; 2023 Homies</p>
+          </div>
+        </footer>
+      </div>
+      <style></style>
+    </>
+  )
+}
+
+
+export default Footer

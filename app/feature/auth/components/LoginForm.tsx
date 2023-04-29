26d211c3fd279bae08cdfca6def8455600562e1c
kou72 <genpokopokopoko@gmail.com>
2023-04-30 02:00:41 +0900

    ログインフォームを作成し、ログインページで表示するように修正
    app/feature/auth/components/LoginForm.tsx
    - ReactとuseStateをインポート
    - emailとpasswordのstateを定義
    - handleSubmit関数を定義し、ログイン処理を行う関数を呼び出す
    - フォームを作成し、emailとpasswordの入力欄を設置
    - ログインボタンを設置
    - LoginFormをエクスポート
    
    app/pages/login.tsx
    - LoginFormをインポート
    - LoginPageコンポーネントを定義し、LoginFormを表示するように修正
    - LoginPageをエクスポート

---
+++ app/feature/auth/components/LoginForm.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    // ログイン処理を行う関数を呼び出す
+    console.log('login')
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="w-full max-w-sm">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Sign In
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default LoginForm

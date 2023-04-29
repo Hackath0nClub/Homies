7f14f8e4d19c4e1956e02c364fc16e07f4322361
kou72 <genpokopokopoko@gmail.com>
2023-04-30 02:56:56 +0900

    Headerコンポーネントのデザインを変更
    Header.tsx
    - ヘッダー全体をflexコンテナに変更
    - ヘッダーの高さを48pxに固定
    - ナビゲーションメニューを中央に配置
    - ナビゲーションメニューのスタイルを変更
    - 不要なスタイル定義を削除

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,85 +1,28 @@
 const Header = (props: any) => {
   return (
     <>
-      <div className={`loged-in-header-container ${props.rootClassName} `}>
-        <header data-role="Header" className="loged-in-header-header">
+      <div className="w-full h-full flex flex-col items-start">
+        <header
+          data-role="Header"
+          className="flex items-center justify-center w-full max-w-full fixed h-[48px] top-0 z-50 pt-2 pl-8 pr-8 pb-2 bg-[rgba(9,26,43,1)]"
+        >
           <img
             alt="pastedImage"
             src="/playground_assets/homies-logo.png"
-            className="loged-in-header-icon"
+            className="h-full"
           />
-          <div className="loged-in-header-nav">
-            <span className="loged-in-header-home">Home</span>
-            <span className="loged-in-header-events">Events</span>
-            <span className="loged-in-header-tickets">Tickets</span>
+          <div className="w-full flex items-center justify-center">
+            <span className="text-white text-md m-4">Home</span>
+            <span className="text-white text-md m-4">Events</span>
+            <span className="text-white text-md m-4">Tickets</span>
           </div>
           <img
             alt="pastedImage"
             src="/playground_assets/user-icon-sample.png"
-            className="loged-in-header-user-icon"
+            className="h-full"
           />
         </header>
       </div>
-      <style jsx>
-        {`
-          .loged-in-header-container {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            position: relative;
-            align-items: flex-start;
-            flex-direction: column;
-          }
-          .loged-in-header-header {
-            width: 100%;
-            height: 48px;
-            display: flex;
-            max-width: 100%;
-            align-items: center;
-            padding-top: 8px;
-            padding-left: 32px;
-            padding-right: 32px;
-            padding-bottom: 8px;
-            justify-content: space-between;
-            background-color: rgba(9, 26, 43, 1);
-            position: fixed;
-            top: 0;
-            z-index: 999;
-          }
-          .loged-in-header-icon {
-            height: 100%;
-          }
-          .loged-in-header-nav {
-            display: flex;
-            position: relative;
-            align-self: center;
-            flex-direction: row;
-            justify-content: center;
-          }
-          .loged-in-header-home {
-            color: rgb(255, 255, 255);
-            font-size: 16px;
-            align-self: stretch;
-            margin-right: 32px;
-          }
-          .loged-in-header-events {
-            color: #ffffff;
-            font-size: 16px;
-            align-self: stretch;
-            margin-right: 32px;
-          }
-          .loged-in-header-tickets {
-            color: #ffffff;
-            font-size: 16px;
-            align-self: stretch;
-            margin-right: 32px;
-          }
-          .loged-in-header-user-icon {
-            width: auto;
-            height: 100%;
-          }
-        `}
-      </style>
     </>
   )
 }

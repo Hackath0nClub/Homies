8003e2ab7bd8c6832411e6f6b19b27504e32e4ed
kou72 <genpokopokopoko@gmail.com>
2023-05-01 00:31:59 +0900

    ログインフォームのコンポーネントをリファクタリングし、useAuthフックを作成した
    LoginForm.tsx
    - handleLogin関数とhandleSignUp関数を削除
    - useStateフックを削除
    - useAuthフックをインポート
    - useAuthフックからemail、password、setEmail、setPassword、handleLogin、handleSignUpを取得
    - handleSubmit関数を削除
    - ボタンのonClickイベントにhandleLoginとhandleSignUpを設定
    - フォームをdivタグに変更
    - テキストボックスの文字色を白に変更
    
    useAuth.tsx
    - useAuthフックを作成
    - useStateフックを使ってemailとpasswordを定義
    - handleLogin関数とhandleSignUp関数を定義
    - handleLogin関数とhandleSignUp関数内で、supabase.auth.signInWithPasswordとsupabase.auth.signUpを使ってログインとサインアップを行う
    - handleLogin関数とhandleSignUp関数内で、エラーが発生した場合はalertでエラーメッセージを表示する
    - email、password、setEmail、setPassword、handleLogin、handleSignUpをエクスポート

--- app/feature/auth/components/LoginForm.tsx
+++ app/feature/auth/components/LoginForm.tsx
@@ -1,50 +1,11 @@
-import React, { useState } from 'react'
-import { supabase } from '../../../utils/supabaseClient'
-import router from 'next/router'
-
-async function handleLogin(email: string, password: string) {
-  try {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    })
-    if (error) throw error
-    router.push('/')
-  } catch (error) {
-    console.error(error)
-    if (error instanceof Error) alert(error.message)
-  }
-}
-
-async function handleSignUp(email: string, password: string) {
-  try {
-    const { data, error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    })
-    if (error) throw error
-    router.push('/')
-  } catch (error) {
-    console.error(error)
-    if (error instanceof Error) alert(error.message)
-  }
-}
+import { useAuth } from '../hooks/useAuth'
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleSubmit = async (e: any, isLogin: boolean) => {
-    e.preventDefault()
-    if (isLogin) {
-      await handleLogin(email, password)
-    } else {
-      await handleSignUp(email, password)
-    }
-  }
+  const { email, password, setEmail, setPassword, handleLogin, handleSignUp } =
+    useAuth()
 
   return (
-    <form className="w-full max-w-sm">
+    <div className="w-full max-w-sm">
       <div className="mb-4">
         <label
           className="block text-gray-700 text-sm font-bold mb-2"
@@ -53,7 +14,7 @@ const LoginForm = () => {
           Email
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
           id="email"
           type="email"
           value={email}
@@ -68,7 +29,7 @@ const LoginForm = () => {
           Password
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
           id="password"
           type="password"
           value={password}
@@ -79,19 +40,19 @@ const LoginForm = () => {
         <button
           className="px-4 py-2 text-white transition-colors duration-300 border border-gray-200 bg-[rgba(28,32,37,1)] rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
           type="submit"
-          onClick={(e) => handleSubmit(e, true)}
+          onClick={handleLogin}
         >
           Sign In
         </button>
         <button
           className="px-4 py-2 text-gray-800 transition-colors duration-300 border border-gray-200 bg-gray-200 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 hover:text-white"
           type="submit"
-          onClick={(e) => handleSubmit(e, false)}
+          onClick={handleSignUp}
         >
           Sign Up
         </button>
       </div>
-    </form>
+    </div>
   )
 }
 
---
+++ app/feature/auth/hooks/useAuth.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { supabase } from '../../../utils/supabaseClient'
+import router from 'next/router'
+
+export const useAuth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async () => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      })
+      console.log(email, password)
+      if (error) throw error
+      router.push('/')
+    } catch (error) {
+      console.error(error)
+      if (error instanceof Error) alert(error.message)
+    }
+  }
+
+  const handleSignUp = async () => {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      })
+      if (error) throw error
+      router.push('/')
+    } catch (error) {
+      console.error(error)
+      if (error instanceof Error) alert(error.message)
+    }
+  }
+
+  return {
+    email,
+    password,
+    setEmail,
+    setPassword,
+    handleLogin,
+    handleSignUp,
+  }
+}

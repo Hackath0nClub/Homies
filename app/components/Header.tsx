af16f70cad6b9f2ccae8bf50e7e3de874884541c
kou72 <genpokopokopoko@gmail.com>
2023-05-06 16:33:30 +0900

    Headerコンポーネントで、サインアウトボタンを追加し、SignupFormコンポーネントを新規作成
    Header.tsx
    - handleSignoutをuseAuthから取得
    - ヘッダーのレイアウトを修正
    - sessionがある場合は、サインアウトボタンを表示するように修正
    - sessionがない場合は、ログイン画面へのリンクを表示するように修正
    
    SignupForm.tsx
    - useAuthから必要な関数と変数を取得
    - ログインフォームのレイアウトを作成
    - ログイン、サインアップ、サインアウトのボタンを作成

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,42 +3,48 @@ import Image from 'next/image'
 import { useAuth } from '../feature/auth/hooks/useAuth'
 
 const Header = () => {
-  const { session } = useAuth()
+  const { session, handleSignout } = useAuth()
   return (
-    <>
-      <div className="w-full h-full flex flex-col items-start">
-        <header
-          data-role="Header"
-          className="flex items-center justify-center w-full max-w-full fixed h-[48px] top-0 z-50 pt-2 pl-8 pr-8 pb-2 bg-[rgba(9,26,43,1)]"
-        >
-          <img
-            alt="pastedImage"
-            src="/playground_assets/homies-logo.png"
-            className="h-full"
-          />
-          <div className="w-full flex items-center justify-center">
-            <span className="text-white text-md m-4">Home</span>
-            <span className="text-white text-md m-4">Events</span>
-            <span className="text-white text-md m-4">Tickets</span>
-          </div>
-          {session && (
-            <Link href="/login">
-              <Image
-                alt="pastedImage"
-                src="/playground_assets/user-icon-sample.png"
-                width={32}
-                height={32}
-              />
-            </Link>
-          )}
-          {!session && (
-            <Link href="/login">
-              <Image alt="pastedImage" src="/user.png" width={32} height={32} />
-            </Link>
-          )}
-        </header>
+    <header
+      data-role="Header"
+      className="flex items-center justify-between w-full max-w-full fixed h-[48px] top-0 z-50 pt-2 pl-8 pr-8 pb-2 bg-[rgba(9,26,43,1)]"
+    >
+      <img
+        alt="pastedImage"
+        src="/playground_assets/homies-logo.png"
+        className="h-full"
+      />
+      <div className="flex">
+        <span className="text-white text-md m-4">Home</span>
+        <span className="text-white text-md m-4">Events</span>
+        <span className="text-white text-md m-4">Tickets</span>
       </div>
-    </>
+      <div className="flex">
+        {session && (
+          <>
+            <Image
+              alt="pastedImage"
+              src="/playground_assets/user-icon-sample.png"
+              width={32}
+              height={32}
+            />
+            <Link href="/login">
+              <span
+                className="w-full mx-2 text-white text-sm"
+                onClick={handleSignout}
+              >
+                Sign Out
+              </span>
+            </Link>
+          </>
+        )}
+        {!session && (
+          <Link href="/login">
+            <Image alt="pastedImage" src="/user.png" width={32} height={32} />
+          </Link>
+        )}
+      </div>
+    </header>
   )
 }
 
---
+++ app/feature/auth/components/SignupForm.tsx
@@ -0,0 +1,73 @@
+import { useAuth } from '../hooks/useAuth'
+
+const LoginForm = () => {
+  const {
+    email,
+    password,
+    setEmail,
+    setPassword,
+    handleLogin,
+    handleSignUp,
+    handleSignout,
+  } = useAuth()
+
+  return (
+    <div className="w-full max-w-sm">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Email
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center gap-4">
+        <button
+          className="px-4 py-2 text-white transition-colors duration-300 border border-gray-200 bg-[rgba(28,32,37,1)] rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+          type="submit"
+          onClick={handleLogin}
+        >
+          Log In
+        </button>
+        <button
+          className="px-4 py-2 text-gray-800 transition-colors duration-300 border border-gray-200 bg-gray-200 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 hover:text-white"
+          type="submit"
+          onClick={handleSignUp}
+        >
+          Sign Up
+        </button>
+        <button
+          className="px-4 py-2 text-white transition-colors duration-300 border border-gray-200 bg-[rgba(28,32,37,1)] rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+          type="submit"
+          onClick={handleSignout}
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm

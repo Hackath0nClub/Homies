25b711b1756bf31ba85f170e5d5fa642983a295b
kou72 <genpokopokopoko@gmail.com>
2023-05-06 18:57:02 +0900

    useAuth.tsxファイルに、routerをインポートして、ログイン・ログアウト・認証エラー時にルートページにリダイレクトするように修正
    useAuth.tsx
    - routerをインポート
    - ログイン・ログアウト・認証エラー時に、router.push('/')を呼び出してルートページにリダイレクトするように修正

--- app/feature/auth/hooks/useAuth.tsx
+++ app/feature/auth/hooks/useAuth.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import router from 'next/router'
 import { useRecoilState } from 'recoil'
 import { sessionState } from '../store/authState'
 import {
@@ -17,18 +18,21 @@ export const useAuth = () => {
     await emailSignIn(email, password)
     const currentSession = await getSession()
     setSession(currentSession)
+    router.push('/')
   }
 
   const handleSignUp = async () => {
     await emailSignUp(email, password)
     const currentSession = await getSession()
     setSession(currentSession)
+    router.push('/')
   }
 
   const handleSignout = async () => {
     await signOut()
     const currentSession = await getSession()
     setSession(currentSession)
+    router.push('/')
   }
 
   return {

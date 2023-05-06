2940e40906940fda7428ace7d28e3c4e19a88e77
kou72 <genpokopokopoko@gmail.com>
2023-05-06 22:06:23 +0900

    EditTimeTableRow.tsxファイルで、UserSearchInputコンポーネントのdefaultValueを修正
    EditTimeTableRow.tsx
    - UserSearchInputコンポーネントのdefaultValueを修正
    - dj.user_idが'@'で始まっていない場合、dj.user_idをdefaultValueに設定するように修正

--- app/feature/event/components/edit/EditTimeTableRow.tsx
+++ app/feature/event/components/edit/EditTimeTableRow.tsx
@@ -164,7 +164,7 @@ const UserSearchInput = ({ index, dj }: { index: number; dj: DjType }) => {
       <input
         type="text"
         className="row-span-1 w-full px-2 block placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
-        defaultValue={dj.user_id}
+        defaultValue={dj.user_id?.charAt(0) != '@' ? dj.user_id : ''}
         onFocus={(e) => {
           handleInputChange(e.target.value)
           setIsOpen(true)

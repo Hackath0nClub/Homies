1d835270ac268edd077bf1a1770e5d12c620136c
kou72 <genpokopokopoko@gmail.com>
2023-05-07 01:46:02 +0900

    イベントページの更新時にアラートを表示するように変更、不要なconsole.logを削除
    app/feature/event/components/edit/UpdateButton.tsx
    - onClickイベントにupdateEvent関数を直接渡すのではなく、アロー関数を使ってupdateEvent関数を呼び出し、その後にアラートを表示するように変更
    
    app/feature/event/hooks/useEvent.tsx
    - fileに関するconsole.logを削除

--- app/feature/event/components/edit/UpdateButton.tsx
+++ app/feature/event/components/edit/UpdateButton.tsx
@@ -5,7 +5,10 @@ export const UpdateButton = () => {
   return (
     <button
       className="border border-white px-20 py-3 gap-2.5 text-white font-bold bg-gradient-to-r from-[rgba(232,112,39,1)] to-[rgba(232,189,39,1)] rounded-3xl transition-transform duration-200 ease-in-out transform hover:scale-95"
-      onClick={updateEvent}
+      onClick={() => {
+        updateEvent()
+        alert('イベントページを更新しました')
+      }}
     >
       イベントページを更新
     </button>

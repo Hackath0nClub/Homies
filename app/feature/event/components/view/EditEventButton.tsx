ffe35f522cb9f09d3e051a42fae6d77c47af4909
kou72 <genpokopokopoko@gmail.com>
2023-07-24 12:44:48 +0900

    EditEventButton.tsxファイルの変更点を分析すると、以下のような変更が行われています。
    - useAuthをimportするためのimport文が追加されました。
    - useEventから取得したbaseとorganizersを変数に代入するように修正されました。
    - useAuthから取得したsessionとauthを変数に代入するように修正されました。
    - イベント編集ボタンの表示条件が変更され、sessionが存在し、authのidがorganizersの最初の要素のuser_idと一致する場合にのみ表示されるように修正されました。
    
    app/feature/event/components/view/EditEventButton.tsx
    - useAuthをimport
    - baseとorganizersを変数に代入
    - sessionとauthを変数に代入
    - イベント編集ボタンの表示条件を修正

--- app/feature/event/components/view/EditEventButton.tsx
+++ app/feature/event/components/view/EditEventButton.tsx
@@ -1,15 +1,22 @@
 import { useEvent } from '../../../../feature/event/hooks/useEvent'
+import { useAuth } from '../../../../feature/auth/hooks/useAuth'
 import Link from 'next/link'
 
 export const EditEventButton = () => {
-  const { base } = useEvent()
+  const { base, organizers } = useEvent()
+  const { session, auth } = useAuth()
+
   return (
-    <div className="w-full text-center my-4">
-      <Link href={'/event/' + base.id + '/edit'}>
-        <button className="border border-white px-16 py-3 text-white font-bold bg-gradient-to-r from-[rgba(232,112,39,1)] to-[rgba(232,189,39,1)] rounded-3xl">
-          イベント編集へ
-        </button>
-      </Link>
-    </div>
+    <>
+      {session && auth.id === organizers[0].user_id ? (
+        <div className="w-full text-center my-4">
+          <Link href={'/event/' + base.id + '/edit'}>
+            <button className="border border-white px-16 py-3 text-white font-bold bg-gradient-to-r from-[rgba(232,112,39,1)] to-[rgba(232,189,39,1)] rounded-3xl">
+              イベント編集へ
+            </button>
+          </Link>
+        </div>
+      ) : null}
+    </>
   )
 }

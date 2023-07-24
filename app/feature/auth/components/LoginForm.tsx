777a0dfcf476b2b824c07b578f8a9843470ee4f8
kou72 <genpokopokopoko@gmail.com>
2023-07-24 11:29:45 +0900

    LoginForm.tsxファイルでボタンのコメントアウトを解除
    LoginForm.tsx
    - ボタンのコメントアウトを解除

--- app/feature/auth/components/LoginForm.tsx
+++ app/feature/auth/components/LoginForm.tsx
@@ -49,13 +49,13 @@ const LoginForm = () => {
       >
         Log In
       </button>
-      <button
+      {/* <button
         className="px-4 py-2 text-white transition-colors duration-300 border border-gray-200 bg-[rgba(28,32,37,1)] rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
         type="submit"
         onClick={handleGoogleLogin}
       >
         Google Log In
-      </button>
+      </button> */}
     </div>
   )
 }
